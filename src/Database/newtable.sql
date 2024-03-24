67bf873822c1722bd31d93e61bc405f371103178
Claudia CECCALDI <106426964+Claudia-Ceccaldi-BTSSIO@users.noreply.github.com>
2024-03-24 11:55:08 +0100

    Add files via upload

--- src/Database/newtable.sql
+++ src/Database/newtable.sql
@@ -1,35 +1,32 @@
+-- Table pour les utilisateurs
 CREATE TABLE Users (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  id_user VARCHAR(255) NOT NULL,
-  password_hash VARCHAR(255) NOT NULL
+  id_user INT AUTO_INCREMENT PRIMARY KEY,
+  username VARCHAR(191) NOT NULL,
+  password_hash VARCHAR(255) NOT NULL,
+  role ENUM('user', 'admin') NOT NULL DEFAULT 'user',
+  registration_code VARCHAR(255),
+  UNIQUE (username(191))
 );
 
-
+-- Table pour le matériel
 CREATE TABLE Materiel (
   id_materiel INT PRIMARY KEY AUTO_INCREMENT,
-  nom VARCHAR(255) NOT NULL,
-  prenom VARCHAR(255) NOT NULL,
-  service VARCHAR(255) NOT NULL,
-  type_select VARCHAR(20),
-  _description VARCHAR(255) NOT NULL,
-  emplacement VARCHAR(255) NOT NULL,
-  annee_uc INT
+  nom_utilisateur VARCHAR(255) NOT NULL,
+  prenom_utilisateur VARCHAR(255) NOT NULL,
+  service_utilisateur VARCHAR(255) NOT NULL,
+  type_materiel VARCHAR(20),
+  description_materiel VARCHAR(255) NOT NULL,
+  emplacement_materiel VARCHAR(255) NOT NULL,
+  annee_materiel INT
 );
 
--- Table pour les attestations de mise à disposition de matériel nomade
-CREATE TABLE AttestationsMateriel (
-  id_attestation INT PRIMARY KEY AUTO_INCREMENT,
-  materiel_tt VARCHAR(255),
-  ecran1_isiac VARCHAR(255),
-  ecran2_isiac VARCHAR(255),
-  uc_isiac VARCHAR(255),
-  enregistre_dans_GACI BOOLEAN,
-  materiel VARCHAR(255),
-  remis_par VARCHAR(255),
-  emprunte_par VARCHAR(255),
-  fonction_emprunteur VARCHAR(255),
-  date_emprunt DATE,
-  date_restitution DATE,
-  recepteur VARCHAR(255),
-  observations VARCHAR(255)
+-- Table pour les demandes d'emprunt
+CREATE TABLE DemandeEmprunt (
+  id_demande INT PRIMARY KEY AUTO_INCREMENT,
+  id_utilisateur INT,
+  id_materiel INT,
+  date_emprunt DATE NOT NULL,
+  observations TEXT,
+  FOREIGN KEY (id_utilisateur) REFERENCES Users(id_user),
+  FOREIGN KEY (id_materiel) REFERENCES Materiel(id_materiel)
 );
