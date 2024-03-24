f7dda70020827d9c8e7e458364675df139e43ba5
Claudia CECCALDI <106426964+Claudia-Ceccaldi-BTSSIO@users.noreply.github.com>
2024-03-24 11:47:56 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,51 @@
+<?php
+
+require_once '../src/Models/databaseConnexion.php';
+
+// Création d'une instance de connexion à la base de données
+$dbConnection = DatabaseConnection::getInstance()->getConnection();
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Application CAF du Lot-et-Garonne</title>
+    <base href="http://localhost/projetcaf/">
+    <link rel="stylesheet" href="public/assets/css/style_index.css?v=1"><!--le ?v=1 force le navigateur à charger la dernière version !-->
+</head>
+
+<body style="background-image: url('http://localhost/projetcaf/public/assets/images/femmepc.jpg'); background-size: cover; 
+background-position: left ; background-repeat: no-repeat; background-attachment: fixed;">
+
+    <div class="container">
+        <div class="hello">
+            <h1>Bienvenue sur l'application de la CAF
+                <br>du Lot-et-Garonne
+            </h1>
+            <h2>Inventaire du Matériel Nomade</h2>
+        </div>
+
+        <div class="button-container">
+            <a href="src/Views/loginView.php" class="btn">Connexion</a>
+        </div>
+        <!-- Bouton d'inscription -->
+        <div class="inscription-container">
+            <a href="src/Views/registerView.php" class="inscription-btn">Inscription</a>
+        </div>
+
+    </div>
+
+</body>
+<footer>
+    <button class="welcome-container btn-zoom" onclick="window.location.href='src/Views/attestMatView.php'">
+        <h2>
+            Attestation de Mise à disposition de Matériel Nomade
+        </h2>
+    </button>
+
+</footer>
+
+</html>
